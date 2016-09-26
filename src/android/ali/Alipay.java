79460d8a913d93ba3e8ace1a352dfec64ec7d1f5
FollowWinter <lyxxxx@yeah.net>
2016-09-26 16:48:54 +0800

    更新支付宝支付

--- src/android/ali/Alipay.java
+++ src/android/ali/Alipay.java
@@ -29,11 +29,24 @@ public class Alipay extends Pay{
                 PayTask alipay = new PayTask(cordova.getActivity());
                 // 调用支付接口，获取支付结果
                 String result = alipay.pay(param, true);
-
-
                 PayResult payResult = new PayResult(result);
-                callbackContext.success();
-                callbackContext.error("error"+result);
+                String resultInfo = payResult.getResult();// 同步返回需要验证的信息
+
+                String resultStatus = payResult.getResultStatus();
+
+                if (TextUtils.equals(resultStatus, "9000")) {
+                    callbackContext.success();
+                } else {
+                    // 判断resultStatus 为非"9000"则代表可能支付失败
+                    // "8000"代表支付结果因为支付渠道原因或者系统原因还在等待支付结果确认，最终交易是否成功以服务端异步通知为准（小概率状态）
+                    if (TextUtils.equals(resultStatus, "8000")) {
+                        Toast.makeText(cordova.getActivity(), "支付结果确认中", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // 其他值就可以判断为支付失败，包括用户主动取消支付，或者系统返回的错误
+                        callbackContext.error("支付失败");
+
+                    }
+                }
             }
         });
 
