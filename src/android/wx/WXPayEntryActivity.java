058cf8e1a6d55cadd440300358721a2785dac236
FollowWinter <lyxxxx@yeah.net>
2016-11-16 12:04:06 +0800

    更新安卓微信支付的回调函数

--- src/android/wx/WXPayEntryActivity.java
+++ src/android/wx/WXPayEntryActivity.java
@@ -1,29 +1,56 @@
-package net.sourceforge.simcpux.wxapi;
+package com.weni.shop.wxapi;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.lyx.pay.wx.Wxpay;
 import com.tencent.mm.sdk.modelbase.BaseReq;
 import com.tencent.mm.sdk.modelbase.BaseResp;
 import com.tencent.mm.sdk.openapi.IWXAPI;
 import com.tencent.mm.sdk.openapi.IWXAPIEventHandler;
+
 import android.app.Activity;
+
 import com.tencent.mm.sdk.openapi.WXAPIFactory;
 
+import org.chromium.base.Log;
+
 public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler {
+
+
+    private static final String TAG = "Weni.WXPayEntryActivity";
+
+    private IWXAPI api;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        api = WXAPIFactory.createWXAPI(this, "wx4fd357e3019b6cd2");
+        api.handleIntent(getIntent(), this);
+        finish();
     }
 
     @Override
     public void onReq(BaseReq baseReq) {
+        Log.e("type","2");
 
     }
 
     @Override
-    public void onResp(BaseResp baseResp) {
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        api.handleIntent(intent, this);
+    }
 
+    @Override
+    public void onResp(BaseResp baseResp) {
+        Log.e("type", baseResp.getType() + "");
+        if (baseResp.getType() == 0) {
+            Wxpay.callbackContext.success();
+        } else {
+            Wxpay.callbackContext.error("error");
+        }
     }
 }
